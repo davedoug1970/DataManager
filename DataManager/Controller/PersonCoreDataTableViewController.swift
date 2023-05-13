fa31c86d11d23ecc3ce0402fc3e1d434a1f35e5c
David Douglas <dave@softwaresmoothie.com>
2023-05-13 12:29:00 -0500

    added delete capability for core data view

--- DataManager/Controller/PersonCoreDataTableViewController.swift
+++ DataManager/Controller/PersonCoreDataTableViewController.swift
@@ -11,17 +11,18 @@ import CoreData
 class PersonCoreDataTableViewController: UITableViewController {
     var people:[NSManagedObject] = []
     
+    // used for core data requests
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var context:NSManagedObjectContext!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load Test Data - if this is your first run through, uncomment below to load
         // some test data.
         //
-        //DataLoader.loadInitialData()
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        
+        // DataLoader.loadInitialData()
+        context = appDelegate.persistentContainer.viewContext
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "PersonCD")
         request.returnsObjectsAsFaults = false
         
@@ -39,7 +40,6 @@ class PersonCoreDataTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return people.count
     }
 
@@ -55,49 +55,30 @@ class PersonCoreDataTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
+    // MARK: - Table view delete support
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            context.delete(people[indexPath.row])
+            do {
+                // apply the delete to core data...
+                try context.save()
+                // remove row from local array...
+                self.people.remove(at: indexPath.row)
+                // update the ui with the change...
+                DispatchQueue.main.async {
+                    self.tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+                print("record deleted successfully")
+            }
+            catch {
+                // error occured in applying the delete to core data
+                print(error.localizedDescription)
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
